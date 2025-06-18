18624fb72136ffcb17599cf077bcec4d0896f400
ProcSheep <3132519428@qq.com>
2025-06-18 18:38:33 +0800

    测试tsx文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const a = "12"
+  console.log(a)
 
   return (
     <>
